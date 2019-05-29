4e44b8857ef70788b839136fbbbb9d00c24498f7
AnselDai <ansel_dai@163.com>
2019-05-29 20:39:50 +0800

    init, add pages, components and .gitignore

---
+++ src/src/data/TempTaskData.js
@@ -0,0 +1,52 @@
+export default {
+  data: [
+    {
+      list: [
+        {
+          id: 0,
+          title: "Task1"
+        },
+        {
+          id: 1,
+          title: "Task2"
+        },
+        {
+          id: 2,
+          title: "Task3"
+        },
+        {
+          id: 3,
+          title: "Task4"
+        },
+        {
+          id: 4,
+          title: "Task5"
+        },
+        {
+          id: 5,
+          title: "Task6"
+        },
+        {
+          id: 6,
+          title: "Task7"
+        },
+        {
+          id: 7,
+          title: "Task8"
+        },
+        {
+          id: 8,
+          title: "Task9"
+        },
+        {
+          id: 9,
+          title: "Task10"
+        },
+        {
+          id: 10,
+          title: "Task11"
+        }
+      ]
+    }
+  ]
+}
