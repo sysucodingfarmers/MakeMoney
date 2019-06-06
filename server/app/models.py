18570d867e514e66b8c906fb16790d3c2a786da9
cxh666 <Chenxhcxh@163.com>
2019-06-06 17:47:38 +0800

    增加task表条目

--- server/app/models.py
+++ server/app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 #用户验证用户是否登录，加入到User的继承中
 from flask_login import UserMixin
+from datetime import datetime, timedelta
 
 class User(UserMixin, db.Model):
 	#学号(账号)
@@ -40,11 +41,26 @@ class Task(db.Model):
 	title = db.Column(db.String(20))
 	#任务类型
 	type = db.Column(db.String(20))
-	#时间
-	strat_time = db()
+	#时间，默认结束时间为5天后
+	strat_time = db.Column(db.DateTime, nullable=False, default=datetime.now())
+	end_time = db.Column(db.DateTime, nullable=False, default=datetime.now()+timedelta(days=5))
+	#报酬
+	pay = 
 	#任务详情
-	detail = db.Column(db.String(100))
-	
+	detail = db.Column(db.Text)
+	#接受者
+	receiver = 
+		#是否完成
+		finished = 
+		#是否支付
+		paid = 
+	#任务人数上限
+	receiver_limit = 
+	#目前完成人数
+	finished_number = 
+	#额外内容
+	extra_content = 
+
 	def __repr__(self):
 		return '<Task {} {}>'.format(self.id, self.title)
 
