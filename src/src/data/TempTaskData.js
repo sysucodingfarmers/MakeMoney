2c72f7dd04c6ac9b0b696b9a29cb825d3ceb13aa
AnselDai <ansel_dai@163.com>
2019-06-10 18:31:40 +0800

    mainPage整体UI优化，增加，修改taskItem组件，修改van-cell源码

--- src/src/data/TempTaskData.js
+++ src/src/data/TempTaskData.js
@@ -6,73 +6,73 @@ export default {
           id: 0,
           title: "Task1",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 1,
           title: "Task2",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 2,
           title: "Task3",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 3,
           title: "Task4",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 4,
-          title: "Task5",
+          title: "A super super super super super super super super super super super super super super long title",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 5,
           title: "Task6",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 6,
           title: "Task7",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 7,
           title: "Task8",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 8,
           title: "Task9",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "fdhfkjsandkasjdfhfkadshfksahfjdsahfkashfkdjshfkdsahfkdjsahfkjshfdkjashfkashdfiwqeuhfqinfvkdnzpihgekafjksafjdkshgvsaindgviweuufjoesnfkdksankf"
         },
         {
           id: 9,
           title: "Task10",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 10,
           title: "Task11",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         },
         {
           id: 10,
           title: "Task12",
           publisher: "Jack",
-          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+          description: "戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐戴馨乐"
         }
       ]
     }
