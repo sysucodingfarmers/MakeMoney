3297a13b78480a52e8636dff543ddf3fb5037df5
AnselDai <ansel_dai@163.com>
2019-06-10 13:26:53 +0800

    修改主页样式，增加组件，修改.gitignore

--- src/src/data/TempTaskData.js
+++ src/src/data/TempTaskData.js
@@ -4,47 +4,75 @@ export default {
       list: [
         {
           id: 0,
-          title: "Task1"
+          title: "Task1",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 1,
-          title: "Task2"
+          title: "Task2",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 2,
-          title: "Task3"
+          title: "Task3",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 3,
-          title: "Task4"
+          title: "Task4",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 4,
-          title: "Task5"
+          title: "Task5",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 5,
-          title: "Task6"
+          title: "Task6",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 6,
-          title: "Task7"
+          title: "Task7",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 7,
-          title: "Task8"
+          title: "Task8",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 8,
-          title: "Task9"
+          title: "Task9",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 9,
-          title: "Task10"
+          title: "Task10",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         },
         {
           id: 10,
-          title: "Task11"
+          title: "Task11",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
+        },
+        {
+          id: 10,
+          title: "Task12",
+          publisher: "Jack",
+          description: "I am a description. fdsafsdfhaskhfdskfhasjkdfhdaskjjdfhdakshfdaskfjhsdajdfhaskjfasdfkhfdsdasfdsafdsafasfdasfdsafdasdfsaf"
         }
       ]
     }
