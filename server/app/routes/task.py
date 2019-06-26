881364d55a77b015aba4acb75906a0c9de6c8fe9
cxh666 <Chenxhcxh@163.com>
2019-06-27 00:37:34 +0800

    用户可以初始化时间；新增查看receiver的状态api，新增可以修改receiver的状态api

--- server/app/routes/task.py
+++ server/app/routes/task.py
@@ -5,6 +5,7 @@ from app import app,db
 from app.models import Template,Answer, Session
 from app.models import User,Task,Receiver,receivers
 from app.utils.trans import UserToJson, TaskToJson
+from datetime import datetime, timedelta
 import os
 import uuid
 import cv2
@@ -30,15 +31,17 @@ def sponsor_task():
         #获得post来的task数据
         json_data = json.loads(request.data)
         print(json_data)
-        print(type(json_data['start_time']))
-        if 'title' not in json_data:
-            return json.dumps({'errmsg': '没有传递title'})
+
+        if 'title' not in json_data or 'pay' not in json_data :
+            return json.dumps({'errmsg': '没有传递title或者没有传递pay'})
+        
+
         task = Task(
             title = json_data['title'],
             sponsor = current_user,
             type = json_data['type'] if 'type' in json_data else '问卷',
-            # start_time = json_data['start_time'] if 'start_time' in json_data else None,
-            # end_time = json_data['end_time'] if 'end_time' in json_data else None,
+            start_time = datetime.strptime(json_data['start_time'],'%Y-%m-%dT%H:%M:%S.%fZ') if 'start_time' in json_data else datetime.now(),
+            end_time = datetime.strptime(json_data['end_time'],'%Y-%m-%dT%H:%M:%S.%fZ') if 'end_time' in json_data else datetime.now()+timedelta(days=10),
             pay = json_data['pay'] if 'pay' in json_data else 0,
             detail = json_data['detail'] if 'detail' in json_data else None,
             receiver_limit = json_data['receiver_limit'] if 'receiver_limit' in json_data else 1,
@@ -47,12 +50,21 @@ def sponsor_task():
             state = 0,
             # images = json_data['images'].encode() if 'images'in json_data else b''
             )
+        #如果账户余额不足则返回错误
+        paySum = task.pay * task.receiver_limit
+        if paySum > current_user.exMoney:
+            return json.dumps({'errmsg': '账户余额不足'})
 
         task.template = Template()
         task.template.questions = json_data['questions'] if 'questions' in json_data else []
         task.template.options = json_data['options'] if 'options' in json_data else []
         task.template.types = json_data['types'] if 'types' in json_data else []
 
+        #任务发布成功，则提前扣除发布者的金额
+        current_user.exMoney = current_user.exMoney - paySum
+        current_user.expend = current_user.expend + paySum
+
+        #提交
         db.session.add(task)
         db.session.commit()
 
@@ -106,7 +118,7 @@ def receive_task():
         #接收任务，修改任务状态
         task.receivers.append(receiver)
         task.received_number += 1
-        if task.state = 0:
+        if task.state == 0:
             task.state = 1
         print(task.receivers)
 
@@ -154,8 +166,15 @@ def task_quit():
             if 'user_id' in json_data:
                 task = Task.query.filter_by(id=json_data['task_id']).first()
                 rec = Receiver.query.filter_by(uid=json_data['user_id'], tid=json_data['task_id']).first()
+                #如果任务已完成，无法退出
+                if task.state == 2:
+                    return json.dumps({'errmsg': '该任务已完成，无法退出'})
                 if (task==None or rec==None):
                     return json.dumps({'errmsg': '用户id或任务id错误'})
+                #如果已完成，则完成人数减一
+                if rec.finished == True:
+                    task.finished_number -= 1
+                #接受人数减一并删除receiver
                 task.received_number -= 1
                 for current_rec in task.receivers:
                     if current_rec.uid == rec.uid:
@@ -230,3 +249,78 @@ def postImage():
             return send_from_directory(app.config['TASK_FOLDER'], filename)
         return json.dumps({'errmsg': '没有传递task_id'})
     return json.dumps({'errmsg': '没有使用POST请求'})
+
+
+'''任务完成
+接受者发出，接受user_id和task_id
+返回True或False
+'''
+@app.route('/task/finished', methods=['POST'])
+def taskFinished():
+    if request.method == 'POST':
+        json_data = json.loads(request.data)
+        if 'user_id' in json_data and 'task_id' in json_data:
+            #查找用户
+            user = User.query.filter_by(id=json_data['user_id']).first()
+            if user==None:
+                return json.dumps({'errmsg': '用户id错误，无该用户'})
+            #查找任务
+            task = Task.query.filter_by(id=json_data['task_id']).first()
+            if task==None:
+                return json.dumps({'errmsg': '任务id错误，无该任务'})
+            #查找接受者
+            receiver = Receiver.query.filter_by(uid=json_data['user_id'], tid=json_data['task_id']).first()
+            if receiver==None:
+                return json.dump({'errmsg': '该用户无接受该任务'})
+            #接受者完成
+            receiver.finished = True
+            #任务完成人数加一
+            task.finished_number = task.finished_number + 1
+            #用户收入以及零钱添加
+
+            db.session.commit()
+            return json_true
+        return json.dumps({'errmsg': '没有传递user_id或没有task_id'})
+    return json.dumps({'errmsg': '没有使用POST请求'})
+
+
+'''支付
+发起者发出，接受user_id和task_id
+返回True或False
+'''
+@app.route('/task/pay', methods=['POST'])
+def pay():
+    if request.method == 'POST':
+        json_data = json.loads(request.data)
+        if 'user_id' in json_data and 'task_id' in json_data:
+            #查找用户
+            user = User.query.filter_by(id=json_data['user_id']).first()
+            if user==None:
+                return json.dumps({'errmsg': '用户id错误，无该用户'})
+            #查找任务
+            task = Task.query.filter_by(id=json_data['task_id']).first()
+            if task==None:
+                return json.dumps({'errmsg': '任务id错误，无该任务'})
+            #用户收钱
+            pay = task.pay
+            user.exMoney = user.exMoney + pay
+            user.income = user.expend + pay
+            task.paid_number = task.paid_number + 1
+
+            db.session.commit()
+
+            #如果任务已经全部支付，则修改任务的state
+            if task.paid_number == task.receiver_limit:
+                task.state = 2
+                db.session.commit()
+                return json.dumps({'msg': '任务已完成'})
+
+            
+            return json_true
+        return json.dumps({'errmsg': '没有传递user_id或没有task_id'})
+    return json.dumps({'errmsg': '没有使用POST请求'})
+
+'''修改任务
+传入要修改的信息
+返回true or false
+'''
--- server/app/routes/user.py
+++ server/app/routes/user.py
@@ -14,7 +14,7 @@ json_false = json.dumps('failed')
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
 
 '''
 接受账号密码和其他个人信息
@@ -24,28 +24,28 @@ def index():
 '''
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	json_data = json.loads(request.data)
-	print(json_data)
-	users = User.query.all()
-	for user in users:
-		if user.id == json_data['id']:
-			print('user is existed!')
-			return json.dumps({'errmsg': '用户已存在'})
-	user = User(id = json_data['id'], 
-		username = json_data['username'], 
-		email = json_data['email'] if 'email' in json_data else None, 
-		school = json_data['school'] if 'school' in json_data else None, 
-		major = json_data['major'] if 'major' in json_data else None, 
-		phone = json_data['phone'] if 'phone' in json_data else None, 
-		wx_number = json_data['wx_number'] if 'wx_number' in json_data else None, 
-		hobbit = json_data['hobbit'] if 'hobbit' in json_data else None
-		)
-	user.set_password(json_data['password'])
-	
-	db.session.add(user)
-	db.session.commit()
-	print('register user {}!'.format(user))
-	return json_true
+    json_data = json.loads(request.data)
+    print(json_data)
+    users = User.query.all()
+    for user in users:
+        if user.id == json_data['id']:
+            print('user is existed!')
+            return json.dumps({'errmsg': '用户已存在'})
+    user = User(id = json_data['id'], 
+        username = json_data['username'], 
+        email = json_data['email'] if 'email' in json_data else None, 
+        school = json_data['school'] if 'school' in json_data else None, 
+        major = json_data['major'] if 'major' in json_data else None, 
+        phone = json_data['phone'] if 'phone' in json_data else None, 
+        wx_number = json_data['wx_number'] if 'wx_number' in json_data else None, 
+        hobbit = json_data['hobbit'] if 'hobbit' in json_data else None
+        )
+    user.set_password(json_data['password'])
+    
+    db.session.add(user)
+    db.session.commit()
+    print('register user {}!'.format(user))
+    return json_true
 
 '''
 登入
@@ -55,89 +55,89 @@ def register():
 '''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		if current_user.is_active:
-			print('Error, user is active!')
-			return json.dumps({'errmsg': '用户已登陆'})
-		#获取post来的数据
-		json_data = json.loads(request.data)
-		user = User.query.filter_by(id=json_data['id']).first()
-		if user is None or not user.check_password(json_data['password']):
-			print('username or password isn\'t correct!')
-			return json.dumps({'errmsg': '用户名或密码错误'})
-		login_user(user)
-		
-		#建立session数据库
-		se = Session.query.filter_by(uid=user.id).first()
-		if se==None:
-			se = Session(uid=user.id)
-			db.session.add(se)
-			db.session.commit()
-			print('Create a new session {}'.format(se.sid))
-		else:
-			print('Resume Session {}'.format(se.sid))
+    if request.method == 'POST':
+        if current_user.is_active:
+            print('Error, user is active!')
+            return json.dumps({'errmsg': '用户已登陆'})
+        #获取post来的数据
+        json_data = json.loads(request.data)
+        user = User.query.filter_by(id=json_data['id']).first()
+        if user is None or not user.check_password(json_data['password']):
+            print('username or password isn\'t correct!')
+            return json.dumps({'errmsg': '用户名或密码错误'})
+        login_user(user)
+        
+        #建立session数据库
+        se = Session.query.filter_by(uid=user.id).first()
+        if se==None:
+            se = Session(uid=user.id)
+            db.session.add(se)
+            db.session.commit()
+            print('Create a new session {}'.format(se.sid))
+        else:
+            print('Resume Session {}'.format(se.sid))
 
-		print('login user {}!'.format(user))
-		data = json.dumps(user, default=UserToJson, sort_keys=False) 
-		
-		#生成返回的resp，增加session的选项
-		resp = make_response()
-		resp.headers['session_id'] = str(se.sid)
-		resp.data = data
-		return resp
-	return json.dumps({'errmsg': '没有使用POST请求'})
+        print('login user {}!'.format(user))
+        data = json.dumps(user, default=UserToJson, sort_keys=False) 
+        
+        #生成返回的resp，增加session的选项
+        resp = make_response()
+        resp.headers['session_id'] = str(se.sid)
+        resp.data = data
+        return resp
+    return json.dumps({'errmsg': '没有使用POST请求'})
 
 #登出
 @app.route('/logout', methods=['POST'])
 def logout():
-	# print(request.)
-	se = Session.query.filter_by(sid=request.session_id, uid=request.user_id).first()
-	#删除session
-	if se!=None:
-		db.session.delete(se)
-		db.session.commit()
-	#登出
-	logout_user()
-	print('logout user succeed!')
-	return json_true
+    # print(request.)
+    se = Session.query.filter_by(sid=request.session_id, uid=request.user_id).first()
+    #删除session
+    if se!=None:
+        db.session.delete(se)
+        db.session.commit()
+    #登出
+    logout_user()
+    print('logout user succeed!')
+    return json_true
 
 '''
 修改用户信息，需要登录
 '''
 @app.route('/modify/user_info', methods=['POST'])
 def modify_user_info():
-	headers = request.headers
-	se = Session.query.filter_by(sid=int(headers['session_id']), uid=int(headers['user_id'])).first()
-	if se==None:
-		print('session is not connected')
-		return json.dumps({'errmsg': '没有建立会话或者会话信息出错'})
-	
-	if request.method == 'POST':
-		json_data = json.loads(request.data)
-		if 'id' in json_data:
-			user = User.query.filter_by(id=json_data['id']).first()
-			print(user)
-			if 'username' in json_data:
-				user.username = json_data['username']
-			if 'email' in json_data:
-				user.email = json_data['email']
-			if 'school' in json_data:
-				user.school = json_data['school']
-			if 'major' in json_data:
-				user.major = json.date['major']
-			if 'phone' in json_data:
-				user.phone = json_data['phone']
-			if 'wx_number' in json_data:
-				user.wx_number = json_data['wx_number']
-			if 'hobbit' in json_data:
-				user.hobbit = json_data['hobbit']
-			if 'profile' in json_data:
-				user.profile = json_data['profile']
-			db.session.commit()
-			return json.dumps(user, default=UserToJson, sort_keys=False) 
+    headers = request.headers
+    se = Session.query.filter_by(sid=int(headers['session_id']), uid=int(headers['user_id'])).first()
+    if se==None:
+        print('session is not connected')
+        return json.dumps({'errmsg': '没有建立会话或者会话信息出错'})
+    
+    if request.method == 'POST':
+        json_data = json.loads(request.data)
+        if 'id' in json_data:
+            user = User.query.filter_by(id=json_data['id']).first()
+            print(user)
+            if 'username' in json_data:
+                user.username = json_data['username']
+            if 'email' in json_data:
+                user.email = json_data['email']
+            if 'school' in json_data:
+                user.school = json_data['school']
+            if 'major' in json_data:
+                user.major = json.date['major']
+            if 'phone' in json_data:
+                user.phone = json_data['phone']
+            if 'wx_number' in json_data:
+                user.wx_number = json_data['wx_number']
+            if 'hobbit' in json_data:
+                user.hobbit = json_data['hobbit']
+            if 'profile' in json_data:
+                user.profile = json_data['profile']
+            db.session.commit()
+            return json.dumps(user, default=UserToJson, sort_keys=False) 
 
-		return json.dumps({'errmsg': '没有指定用户'})
-	return json.dumps({'errmsg': '没有使用POST请求'})
+        return json.dumps({'errmsg': '没有指定用户'})
+    return json.dumps({'errmsg': '没有使用POST请求'})
 
 '''上传头像
 接受用户的id
@@ -167,7 +167,12 @@ def postProfile():
             return send_from_directory(app.config['PROFILE_FOLDER'], filename)
         return json.dumps({'errmsg': '没有传递user_id'})
     return json.dumps({'errmsg': '没有使用POST请求'})
-#查看钱包状态
+
+
+'''查看钱包状态
+传入user_id
+返回钱包的余额，收入和支出
+'''
 @app.route('/wallet', methods=['POST'])
 def wallet():
     if request.method == 'POST':
@@ -181,7 +186,10 @@ def wallet():
         return json.dumps({'errmsg': '没有传递user_id'})
     return json.dumps({'errmsg': '没有使用POST请求'})
 
-#充值
+'''充值
+传入user_id和value
+返回账户金额
+'''
 @app.route('/recharge', methods=['POST'])
 def recharge():
     if request.method == 'POST':
@@ -199,9 +207,12 @@ def recharge():
         return json.dumps({'errmsg': '没有传递user_id或没有传递充值金额value'})
     return json.dumps({'errmsg': '没有使用POST请求'})
 
-#提现
-@app.route('/recharge', methods=['POST'])
-def recharge():
+'''提现
+传入user_id和value
+返回账户金额
+'''
+@app.route('/withdraw', methods=['POST'])
+def withdraw():
     if request.method == 'POST':
         json_data = json.loads(request.data)
         if 'user_id' in json_data and 'value' in json_data:
@@ -211,7 +222,7 @@ def recharge():
                 return json.dumps({'errmsg': '用户id错误，无该用户'})
             print(user.exMoney)
             if user.exMoney - json_data['value'] < 0:
-            	return json.dumps({'errmsg': '账户余额不足'})
+                return json.dumps({'errmsg': '账户余额不足'})
             user.exMoney = user.exMoney - json_data['value']
             db.session.commit()
             print(user.exMoney)
